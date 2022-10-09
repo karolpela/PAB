ab37f82df6ffa2e89043ba0800c9c9584b9efda6
irseq <11260208+irseq@users.noreply.github.com>
2022-10-10 01:12:28 +0200

    Simplify code with Groovy features

--- Groovy_1/src/ex4/Main.groovy
+++ Groovy_1/src/ex4/Main.groovy
@@ -1,23 +1,21 @@
 package ex4
 
-static void main(String[] args) {
-    def numbersToWords =
-            [1: "jeden",
-             2: "dwa",
-             3: "trzy",
-             4: "cztery",
-             5: "pięć",
-             6: "sześć",
-             7: "siedem",
-             8: "osiem",
-             9: "dziewięć"]
-    print "Enter a number: "
-    def input = System.in.newReader().readLine()
+def numbersToWords =
+        [1: "jeden",
+         2: "dwa",
+         3: "trzy",
+         4: "cztery",
+         5: "pięć",
+         6: "sześć",
+         7: "siedem",
+         8: "osiem",
+         9: "dziewięć"]
+print "Enter a number: "
+def input = System.in.newReader().readLine()
 
-    if (input.isNumber()) {
-        def numbers = input.getChars()
-                .collect { Character.getNumericValue(it) }
-        def words = numbers.collect { numbersToWords[it] }
-        println(words.join(" - "))
-    }
+if (input.isNumber()) {
+    def numbers = input.getChars()
+            .collect { Character.getNumericValue(it) }
+    def words = numbers.collect { numbersToWords[it] }
+    println words.join(" - ")
 }
