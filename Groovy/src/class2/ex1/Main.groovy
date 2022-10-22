560c3ce50cc876afb3b8c216dad5daaec96a959e
irseq <11260208+irseq@users.noreply.github.com>
2022-10-22 15:53:07 +0200

    Finish task 2.1

---
+++ Groovy/src/class2/ex1/Main.groovy
@@ -0,0 +1,24 @@
+package class2.ex1
+
+(maxVal, indList) = maximum([5, 3, 5, 5, 1, 5])
+println "Maximum: $maxVal"
+println "Indexes: $indList"
+
+(maxVal, indList) = maximum(['a', 'c', 'c', 'c', 'b'])
+println "Maximum: $maxVal"
+println "Indexes: $indList"
+
+static def maximum(List list) {
+    def currMax = 0
+    def indices = new ArrayList<>()
+    list.eachWithIndex { entry, int i ->
+        if (entry >= currMax) {
+            if (entry > currMax) {
+                indices.clear()
+                currMax = entry
+            }
+            indices.add(i)
+        }
+    }
+    return [currMax, indices]
+}
